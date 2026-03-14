63bb7650a4ae98ea33d9e28a1f815f41370937d4
KaiSevelin <61228732+KaiSevelin@users.noreply.github.com>
2026-03-14 15:02:35 +0100

    werrrrrrrrrrrrrrrrrrrr

---
+++ modules/chargen1547_v2/cards/cards.js
@@ -0,0 +1,480 @@
+﻿export const CARD_IMAGES = [
+    {
+        "name":  "Cosmopolitan",
+        "path":  "media/home/games/1547/Cards/Culture Cosmopolitan.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Court",
+        "path":  "media/home/games/1547/Cards/Culture Court.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Esoteric",
+        "path":  "media/home/games/1547/Cards/Culture Esoteric.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Exceptional",
+        "path":  "media/home/games/1547/Cards/Culture Exceptional.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Exiled",
+        "path":  "media/home/games/1547/Cards/Culture Exiled.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Guild",
+        "path":  "media/home/games/1547/Cards/Culture Guild.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Market",
+        "path":  "media/home/games/1547/Cards/Culture Market.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Military",
+        "path":  "media/home/games/1547/Cards/Culture Military.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Monastic",
+        "path":  "media/home/games/1547/Cards/Culture Monastic.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Remote",
+        "path":  "media/home/games/1547/Cards/Culture Remote.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "River",
+        "path":  "media/home/games/1547/Cards/Culture River.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Rural parish",
+        "path":  "media/home/games/1547/Cards/Culture Rural parish.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "University",
+        "path":  "media/home/games/1547/Cards/Culture University.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Urban",
+        "path":  "media/home/games/1547/Cards/Culture Urban.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Zone borderlands",
+        "path":  "media/home/games/1547/Cards/Culture Zone borderlands.webp",
+        "category":  "Culture"
+    },
+    {
+        "name":  "Ambition",
+        "path":  "media/home/games/1547/Cards/General Ambition.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Authority",
+        "path":  "media/home/games/1547/Cards/General Authority.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Betrayal",
+        "path":  "media/home/games/1547/Cards/General Betrayal.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Birth",
+        "path":  "media/home/games/1547/Cards/General Birth.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Bloodline",
+        "path":  "media/home/games/1547/Cards/General Bloodline.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Compromise",
+        "path":  "media/home/games/1547/Cards/General Compromise.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Conflict",
+        "path":  "media/home/games/1547/Cards/General Conflict.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Corruption",
+        "path":  "media/home/games/1547/Cards/General Corruption.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Craft",
+        "path":  "media/home/games/1547/Cards/General Craft.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Death",
+        "path":  "media/home/games/1547/Cards/General Death.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Delay",
+        "path":  "media/home/games/1547/Cards/General Delay.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Disease",
+        "path":  "media/home/games/1547/Cards/General Disease.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Dreams",
+        "path":  "media/home/games/1547/Cards/General Dreams.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Expectation",
+        "path":  "media/home/games/1547/Cards/General Expectation.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Faith",
+        "path":  "media/home/games/1547/Cards/General Faith.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Fate",
+        "path":  "media/home/games/1547/Cards/General Fate.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Grief",
+        "path":  "media/home/games/1547/Cards/General Grief.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Honor",
+        "path":  "media/home/games/1547/Cards/General Honor.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Horror",
+        "path":  "media/home/games/1547/Cards/General Horror.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Judgment",
+        "path":  "media/home/games/1547/Cards/General Judgment.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Love",
+        "path":  "media/home/games/1547/Cards/General Love.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Magic",
+        "path":  "media/home/games/1547/Cards/General Magic.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Miracle",
+        "path":  "media/home/games/1547/Cards/General Miracle.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Nature",
+        "path":  "media/home/games/1547/Cards/General Nature.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Obligation",
+        "path":  "media/home/games/1547/Cards/General Obligation.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Omen",
+        "path":  "media/home/games/1547/Cards/General Omen.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Oppression",
+        "path":  "media/home/games/1547/Cards/General Oppression.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Planning",
+        "path":  "media/home/games/1547/Cards/General Planning.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Public life",
+        "path":  "media/home/games/1547/Cards/General Public life.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Reputation",
+        "path":  "media/home/games/1547/Cards/General Reputation.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Revenge",
+        "path":  "media/home/games/1547/Cards/General Revenge.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Risk",
+        "path":  "media/home/games/1547/Cards/General Risk.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Scarcity",
+        "path":  "media/home/games/1547/Cards/General Scarcity.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Secrets",
+        "path":  "media/home/games/1547/Cards/General Secrets.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Study",
+        "path":  "media/home/games/1547/Cards/General Study.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Training",
+        "path":  "media/home/games/1547/Cards/General Training.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Transformation",
+        "path":  "media/home/games/1547/Cards/General Transformation.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Travel",
+        "path":  "media/home/games/1547/Cards/General Travel.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Unknown",
+        "path":  "media/home/games/1547/Cards/General Unknown.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "War",
+        "path":  "media/home/games/1547/Cards/General War.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Wealth",
+        "path":  "media/home/games/1547/Cards/General Wealth.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Aquarius",
+        "path":  "media/home/games/1547/Cards/Horoscope Aquarius.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Aries",
+        "path":  "media/home/games/1547/Cards/Horoscope Aries.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Cancer",
+        "path":  "media/home/games/1547/Cards/Horoscope Cancer.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Capricorn",
+        "path":  "media/home/games/1547/Cards/Horoscope Capricorn.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Gemini",
+        "path":  "media/home/games/1547/Cards/Horoscope Gemini.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Leo",
+        "path":  "media/home/games/1547/Cards/Horoscope Leo.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Libra",
+        "path":  "media/home/games/1547/Cards/Horoscope Libra.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Pisces",
+        "path":  "media/home/games/1547/Cards/Horoscope Pisces.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Sagittarius",
+        "path":  "media/home/games/1547/Cards/Horoscope Sagittarius.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Scorpio",
+        "path":  "media/home/games/1547/Cards/Horoscope Scorpio.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Taurus",
+        "path":  "media/home/games/1547/Cards/Horoscope Taurus.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Virgo",
+        "path":  "media/home/games/1547/Cards/Horoscope Virgo.webp",
+        "category":  "Horoscope"
+    },
+    {
+        "name":  "Balanced",
+        "path":  "media/home/games/1547/Cards/Humors Balanced.webp",
+        "category":  "Humors"
+    },
+    {
+        "name":  "Black bile",
+        "path":  "media/home/games/1547/Cards/Humors Black bile.webp",
+        "category":  "Humors"
+    },
+    {
+        "name":  "Blood",
+        "path":  "media/home/games/1547/Cards/Humors Blood.webp",
+        "category":  "Humors"
+    },
+    {
+        "name":  "Mixed",
+        "path":  "media/home/games/1547/Cards/Humors Mixed.webp",
+        "category":  "Humors"
+    },
+    {
+        "name":  "Mystery",
+        "path":  "media/home/games/1547/Cards/Humors Mystery.webp",
+        "category":  "Humors"
+    },
+    {
+        "name":  "Phlegm",
+        "path":  "media/home/games/1547/Cards/Humors Phlegm.webp",
+        "category":  "Humors"
+    },
+    {
+        "name":  "Unbalanced",
+        "path":  "media/home/games/1547/Cards/Humors Unbalanced.webp",
+        "category":  "Humors"
+    },
+    {
+        "name":  "Yellow bile",
+        "path":  "media/home/games/1547/Cards/Humors Yellow bile.webp",
+        "category":  "Humors"
+    },
+    {
+        "name":  "Born alone",
+        "path":  "media/home/games/1547/Cards/Origin Born alone.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born apprentice",
+        "path":  "media/home/games/1547/Cards/Origin Born apprentice.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born clerk",
+        "path":  "media/home/games/1547/Cards/Origin Born clerk.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born court functionary",
+        "path":  "media/home/games/1547/Cards/Origin Born court functionary.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born fisherfolk",
+        "path":  "media/home/games/1547/Cards/Origin Born fisherfolk.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born guildsman",
+        "path":  "media/home/games/1547/Cards/Origin Born guildsman.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born high noble",
+        "path":  "media/home/games/1547/Cards/Origin Born high noble.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born knightly",
+        "path":  "media/home/games/1547/Cards/Origin Born knightly.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born laborer",
+        "path":  "media/home/games/1547/Cards/Origin Born laborer.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born lesser gentry",
+        "path":  "media/home/games/1547/Cards/Origin Born lesser gentry.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born military",
+        "path":  "media/home/games/1547/Cards/Origin Born military.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born petty bourgeois",
+        "path":  "media/home/games/1547/Cards/Origin Born petty bourgeois.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born royal bastard",
+        "path":  "media/home/games/1547/Cards/Origin Born royal bastard.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born serf",
+        "path":  "media/home/games/1547/Cards/Origin Born serf.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born tenant farmer",
+        "path":  "media/home/games/1547/Cards/Origin Born tenant farmer.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "Born trader",
+        "path":  "media/home/games/1547/Cards/Origin Born trader.webp",
+        "category":  "Origin"
+    },
+    {
+        "name":  "backside",
+        "path":  "media/home/games/1547/Cards/backside.webp",
+        "category":  "backside"
+    },
+    {
+        "name":  "Opportunity",
+        "path":  "media/home/games/1547/Cards/General Opportunity.webp",
+        "category":  "General"
+    },
+    {
+        "name":  "Coastal",
+        "path":  "media/home/games/1547/Cards/Culture Coastal.webp",
+        "category":  "Culture"
+    }
+]
+
+export default CARD_IMAGES;
+
---
+++ modules/chargen1547_v2/foundry rolltables/rolltables.js
@@ -0,0 +1,346 @@
+﻿export const FOUNDRY_ROLLTABLES = [
+    {
+        "name":  "Adolescence - Apprenticeship",
+        "uuid":  "RollTable.UQbxrFgmwIhF7C6Z",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Adolescence - Turning Point",
+        "uuid":  "RollTable.FLfMZ9iohHhxtU1I",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Alchemist",
+        "uuid":  "RollTable.sFt2zfogDamQFL46",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Artist",
+        "uuid":  "RollTable.kEWbGLBm0Fn264Wz",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Assassin",
+        "uuid":  "RollTable.ING1R5qxA3CZlzw7",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Cavalry",
+        "uuid":  "RollTable.wAgGiKah42jPkc3A",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Courtier",
+        "uuid":  "RollTable.ijcOO4ESiaen56Yu",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Cunning Folk",
+        "uuid":  "RollTable.xwT9rbEtaCplBKtU",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Explorer",
+        "uuid":  "RollTable.VT99LBJzDu8vqFQO",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Guildmaster",
+        "uuid":  "RollTable.wW3X8NEJ00oYwUab",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Imprisoned",
+        "uuid":  "RollTable.h3GsLBBu1ovDua9o",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Inquisitioner",
+        "uuid":  "RollTable.3NwgqNEi3CUf1gLa",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Knight",
+        "uuid":  "RollTable.UI0dXws7eVib8ck3",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Master Thief",
+        "uuid":  "RollTable.nRWxxU4aIZ6ng2vH",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Musketeer",
+        "uuid":  "RollTable.U2xSPqFsg17BLsWE",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Occultist",
+        "uuid":  "RollTable.ZgnfOdgm32rS1gmv",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Officer",
+        "uuid":  "RollTable.WMuB1MNtzlqKZgJX",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Advanced - Veteran",
+        "uuid":  "RollTable.f32EHBGDAdPR68Km",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Birth - Culture",
+        "uuid":  "RollTable.xAv1PpHtLBk0okve",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Birth - Family status",
+        "uuid":  "RollTable.MQ2aScVM4KOCZvqI",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Birth - Origin",
+        "uuid":  "RollTable.9nGKQuiE0n8zMKis",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Birth - Horoscope (3d6)",
+        "uuid":  "RollTable.WqxPqlsw4LlVk5mp",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Birth - Humors",
+        "uuid":  "RollTable.DeL6AoYlpbZdonNB",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Blessings",
+        "uuid":  "RollTable.T9L3nhbOJJDslvAE",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Bloodline powers",
+        "uuid":  "RollTable.eBpNPLiNagcj7dAB",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Bodily Changes",
+        "uuid":  "RollTable.0lsu5sVbypU2KplI",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Archer",
+        "uuid":  "RollTable.bp9WEbKRooNKQvGt",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Craftsman",
+        "uuid":  "RollTable.IoGU4kOWXz9PSFOK",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Criminal",
+        "uuid":  "RollTable.X55z5fpz3bTdZClp",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Farmer",
+        "uuid":  "RollTable.A1RrLCICrK5f40jJ",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Hunter",
+        "uuid":  "RollTable.MrV9BCMG2bqx79nv",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Infantry",
+        "uuid":  "RollTable.KDDwoJJO2TqLfVAC",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Laborer",
+        "uuid":  "RollTable.mpCZ3Ol8CZc7f5SI",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Mercenary",
+        "uuid":  "RollTable.ntwdFBjHDGPRbTGp",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Merchant",
+        "uuid":  "RollTable.JCTyoDEhUK9QI2F9",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Physician",
+        "uuid":  "RollTable.ueXoInj2HzzNOmoB",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Poor",
+        "uuid":  "RollTable.qs5qcuXODy67x8m1",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Religious",
+        "uuid":  "RollTable.QBV98dNvE1QvrOm9",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Sailor",
+        "uuid":  "RollTable.K3kispvuhYBseRZi",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Scholar",
+        "uuid":  "RollTable.YascVYcBBd6ay4eM",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Career - Squire",
+        "uuid":  "RollTable.bxLsTYFLJjVsjO2t",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Childhood - Event",
+        "uuid":  "RollTable.lYCgeYXWr1ffGGfP",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Contact Flavor",
+        "uuid":  "RollTable.YBiS5eK7mf7v6V4s",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Contact Hook",
+        "uuid":  "RollTable.hiuQ0wJLCNc7reK7",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Contact Quirk",
+        "uuid":  "RollTable.akVp6Ju3EW80CP3N",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Contact Roles",
+        "uuid":  "RollTable.BvPhlA2uAb4uo0Ni",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Contact Tone",
+        "uuid":  "RollTable.WGJQlBQ1HgfzMhaY",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Curses",
+        "uuid":  "RollTable.XJp7FU4UxUzU4261",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Esteem",
+        "uuid":  "RollTable.kC0YeELeXOdwDeaJ",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Coastal",
+        "uuid":  "RollTable.LldfCZKq168ymqIO",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Craft",
+        "uuid":  "RollTable.Ry35kEjiCIOdVbM7",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Criminal",
+        "uuid":  "RollTable.tTWw80p9Bk6KoCko",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Household Items",
+        "uuid":  "RollTable.e6kRkeap9Cz1talC",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Luxury",
+        "uuid":  "RollTable.3rEWQziu0PS0OIQo",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Military",
+        "uuid":  "RollTable.4Xbki12nYfJHAIdX",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Occult",
+        "uuid":  "RollTable.weXLb9rAqFMsHe6W",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Officer",
+        "uuid":  "RollTable.YPAgLzZ6GaqIATp4",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Outdoor",
+        "uuid":  "RollTable.GMW7FKtzQpNC8SZq",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Rural",
+        "uuid":  "RollTable.OIqIsnUMlK9xmdts",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Sacral",
+        "uuid":  "RollTable.8ikUzKO551glTfOg",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Scholar",
+        "uuid":  "RollTable.rTFMoilY2MVJcsiE",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Trade",
+        "uuid":  "RollTable.02C8vUNaIquqBEig",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Items - Urban",
+        "uuid":  "RollTable.5ZlGGE503AIGn0kW",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Luck",
+        "uuid":  "RollTable.mWI6zmHkHhQA84Yp",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Spells Major",
+        "uuid":  "RollTable.9YHAMJK1oa0XLOkD",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Spells Medium",
+        "uuid":  "RollTable.95Ej79OHMJ9nBqgD",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Spells Minor",
+        "uuid":  "RollTable.L6GjkyWmF3QsMezU",
+        "folder":  "CharacterGeneration"
+    },
+    {
+        "name":  "Suspicion",
+        "uuid":  "RollTable.wc0827FvVWKn4eJO",
+        "folder":  "CharacterGeneration"
+    }
+]
+
+export default FOUNDRY_ROLLTABLES;
+
+
